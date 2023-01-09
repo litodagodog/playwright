f4e99667d1d416f20c1669195a6c3dbe54b9557c
Lito Enriquez Dagodog <lito@lanexus.com>
2023-01-09 15:27:24 +0800

    update

--- e2e/test.js
+++ e2e/test.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Installation', async ({ page }) => {
+test('Menu Testing', async ({ page }) => {
   const URL = 'https://playwright.dev/docs/intro';
   await page.goto(URL);
   const response = await page.request.get(URL);
@@ -9,8 +9,14 @@ test('Installation', async ({ page }) => {
   await page.getByRole('link', { name: 'Installation' }).click();
   await expect(page.getByRole('heading', { name: 'Installation' })).toHaveText('Installation');
   await expect(page.getByRole('heading', { name: 'Installing Playwright#'})).toHaveText('Installing Playwright');
+  await page.getByRole('link', { name: 'Writing Tests' }).nth(0).click();
+  await expect(page.getByRole('heading', { name: 'Writing Tests' })).toHaveText('Writing Tests');
+  await page.getByRole('link', { name: 'Running Tests' }).nth(0).click();
+  await expect(page.getByRole('heading', { name: 'Running Tests' })).toHaveText('Running Tests');
+  await page.getByRole('link', { name: 'Test Generator' }).nth(0).click();
+  await expect(page.getByRole('heading', { name: 'Test Generator' })).toHaveText('Test Generator');
 });
-test('Assersions', async ({ page }) => {
+test.skip('Assersions', async ({ page }) => {
     const URL = 'https://playwright.dev/docs/test-assertions';
     await page.goto(URL);
     const response = await page.request.get(URL);
---
+++ report
@@ -0,0 +1,1 @@
+npx playwright show-report
