41f186b33bc3e1c88630273bc4519c4cd71e1db7
Lito Enriquez Dagodog <lito@lanexus.com>
2023-02-03 09:36:43 +0800

    update

--- e2e/eChoice_Diagnoses.ts
+++ e2e/eChoice_Diagnoses.ts
@@ -35,7 +35,7 @@ test('Select Resident', async () => {
 });
 
 test('Diagnoses', async () => {
-    //ADD
+    //Open Add Diagnosis modal
     await page.getByRole('button', { name: 'Open' }).click();
     await page.getByRole('option', { name: /Diagnoses .*/ }).click();
     await page.waitForTimeout(10000);
@@ -43,7 +43,8 @@ test('Diagnoses', async () => {
     await page.getByRole('heading', { name: 'Add Diagnosis' }).isVisible();
     await page.getByPlaceholder('Search Diagnosis').click();
     await page.getByRole('option', { name: 'Percutaneous aspiration of renal pelvis' }).click();
-    
+    await page.waitForTimeout(9000);
+    //Check for Duplicate Entry and Delete it
     const duplicateEntry = await page.getByText('Duplicate Diagnosis').isVisible();
     if ((duplicateEntry == true )){
       await page.getByRole('button', { name: 'CANCEL' }).click();
@@ -53,21 +54,56 @@ test('Diagnoses', async () => {
       await page.waitForLoadState();
     }
     else {
+      //Add Diagnosis
       await page.getByRole('button', { name: 'ADD' }).isEnabled();
       await page.getByRole('button', { name: 'ADD' }).click();
-      // await page.waitForTimeout(8000);
+      await page.waitForTimeout(8000);
       await page.getByRole('cell', { name: 'Percutaneous aspiration of renal pelvis' }).isVisible();
       await page.getByRole('cell', { name: 'This note is from playwright automation' }).isVisible();
+      //Update Diagnosis
+      await page.getByRole('row', { name: /Percutaneous aspiration of renal pelvis .*/ }).locator('#fade-button').click();
+      await page.getByRole('menuitem', { name: 'Update' }).click();
+      await page.getByRole('heading', { name: 'Update Diagnosis' }).isVisible();
+      await page.getByPlaceholder('Search Diagnosis').click();
+      await page.getByPlaceholder('Search Diagnosis').fill('Adult');
+      await page.waitForTimeout(15000);
+      await page.getByRole('option', { name: 'Adult osteochondritis of spine' }).click();
+      await page.getByLabel('Notes').click();
+      await page.getByLabel('Notes').fill('Updated Notes using playwright');
+      await page.getByRole('button', { name: 'UPDATE' }).click();
+      await page.getByText('Successfully updated Diagnosis.').isVisible();
+      await page.waitForTimeout(8000);
+      await page.getByRole('cell', { name: 'Adult osteochondritis of spine' }).click();
+      await page.getByRole('cell', { name: 'Updated Notes using playwright' }).click();
+      //Cease Diagnosis
+      await page.getByRole('row', { name: /Adult osteochondritis of spine .*/ }).locator('#fade-button').click();
+      await page.getByRole('menuitem', { name: 'Cease' }).click();
+      await page.getByRole('heading', { name: 'Cease Diagnosis?' }).click();
+      await page.getByRole('button', { name: 'CEASE' }).click();
+      await page.getByText('Successfully ceased Diagnosis.').isVisible();
+      await page.waitForTimeout(8000);
+      await page.getByRole('tab', { name: 'Ceased' }).click();
+      await page.getByRole('cell', { name: 'Adult osteochondritis of spine' }).isVisible();
+      //Uncease Diagnosis
+      await page.getByRole('row', { name: /Adult osteochondritis of spine .*/ }).locator('#fade-button').click();
+      await page.getByRole('menuitem', { name: 'Uncease' }).click();
+      await page.getByRole('heading', { name: 'Uncease Diagnosis?' }).click();
+      await page.getByRole('button', { name: 'UNCEASE' }).click();
+      await page.getByText('Succesfully unceased Diagnosis.').isVisible();
+      await page.waitForTimeout(8000);
+      //Delete Diagnosis
+      await page.getByRole('tab', { name: 'Active' }).click();
+      await page.getByRole('cell', { name: 'Adult osteochondritis of spine' }).isVisible();
+      await page.getByRole('row', { name: 'Adult osteochondritis of spine 03/02/2023 Updated Notes using playwright 03/02/2023 08:48 AM Diane Curtis' }).locator('#fade-button').click();
+      await page.getByText('Delete').click();
+      await page.getByRole('heading', { name: 'Delete Diagnosis?' }).isVisible();
+      await page.getByRole('button', { name: 'DELETE' }).click();
+      await page.waitForTimeout(8000);
+      await page.getByRole('cell', { name: 'Adult osteochondritis of spine' }).isHidden();
+      await page.getByRole('tab', { name: 'Ceased' }).click();
+      await page.waitForTimeout(8000);
+      await page.getByRole('cell', { name: 'Adult osteochondritis of spine' }).isHidden();
     }
+
+
 });
-
-
-// await page.getByRole('option', { name: 'Diagnoses (2) Updated 02/02/2023' }).click();
-// await page.getByRole('button', { name: 'ADD DIAGNOSIS' }).click();
-// await page.getByPlaceholder('Search Diagnosis').click();
-// await page.getByPlaceholder('Search Diagnosis').fill('per');
-// await page.getByRole('option', { name: 'Percutaneous aspiration of renal pelvis' }).click();
-// await page.getByText('Duplicate Diagnosis').click();
-// await page.getByPlaceholder('Search Diagnosis').click();
-// await page.getByRole('option', { name: 'Sycosis' }).click();
-// await page.getByRole('button', { name: 'ADD' }).click();
