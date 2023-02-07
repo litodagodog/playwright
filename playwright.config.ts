5b74b135d1ac20243790f10cbb3115a63c4a5bb2
Lito Enriquez Dagodog <lito@lanexus.com>
2023-02-07 10:00:27 +0800

    working scripts

--- playwright.config.ts
+++ playwright.config.ts
@@ -62,19 +62,20 @@ const config: PlaywrightTestConfig = {
       },
     },
 
-    {
+    /* {
       name: 'firefox',
       use: {
         ...devices['Desktop Firefox'],
       },
     },
 
-    /*{
+    {
       name: 'webkit',
       use: {
         ...devices['Desktop Safari'],
       },
-    }, */
+    }, 
+    */
 
     /* Test against mobile viewports. */
     // {
